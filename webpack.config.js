3831c2a685a96827da3afacca42c09ef1133665f
johnlyden <john.lyden86@gmail.com>
2017-12-18 19:37:17 -0600

    chnaged webpack.config to build bundle into src

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ module.exports = {
     './src/index.js'
   ],
   output: {
-    path: __dirname,
-    publicPath: '/',
+    path: __dirname + "/src",
+    publicPath: "/",
     filename: 'bundle.js'
   },
   module: {
